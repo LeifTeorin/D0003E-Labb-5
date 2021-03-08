ab7f145f02a79d7989bc4695931b941165d1e670
LeifTeorin <hjalle2000@gmail.com>
2021-03-08 10:43:03 +0100

    Initial commit

---
+++ Labb5/Labb5/main.c
@@ -0,0 +1,18 @@
+/*
+ * Labb5.c
+ *
+ * Created: 2021-03-08 10:41:56
+ * Author : hjall
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
